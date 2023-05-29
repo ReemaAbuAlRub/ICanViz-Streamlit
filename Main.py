7701e0a7d02f09e30a508c45aa2ce7db4c8b9e7f
Reema Abu Al Rub <108832550+ReemaAbuAlRub@users.noreply.github.com>
2023-05-29 21:31:27 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -17,16 +17,35 @@ from gspread_pandas import Spread,Client
 from google.oauth2 import service_account
 from gsheetsdb import connect
 
-import csv
-import json
-
-
 
 DSUC1=pd.read_csv('DSUC1.csv')
 DSUC5=pd.read_csv('DSUC5.csv')
 
 st.set_page_config(page_title='iCanViz',page_icon='logo_rotated.png',layout='wide')
 
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
 def spaces(num):
   for i in range(num):
     st.write('\n')
